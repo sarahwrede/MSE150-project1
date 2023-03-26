14b253f00977f848b04b7e9626278d6f43d69e07
Sarah Wrede <sarahwrede@u.boisestate.edu>
2023-03-26 14:40:50 -0600

    changed vstack to stack

--- plot.py
+++ plot.py
@@ -7,8 +7,8 @@ data1 = np.loadtxt(filename, skiprows=32, max_rows=1000, delimiter=" ", usecols=
 data2 = np.loadtxt(filename, skiprows=1051, max_rows=1000, delimiter=" ", usecols=(1,5))
 data3 = np.loadtxt(filename, skiprows=2070, max_rows=1000, delimiter=" ", usecols=(1,5))
 
-stress = np.vstack((data1[:, 0], data2[:, 0], data3[:, 0]))
-strain = np.vstack((data1[:, 1], data2[:, 1], data3[:, 1]))
+stress = np.stack((data1[:, 0], data2[:, 0], data3[:, 0]))
+strain = np.stack((data1[:, 1], data2[:, 1], data3[:, 1]))
 plt.plot(strain, stress)
 plt.xlabel('Strain (%)')
 plt.ylabel('Stress (Pa)')
@@ -16,7 +16,7 @@ plt.title('Stress vs. Strain')
 
 max_index = np.argmax(np.gradient(stress))
 linear_range = slice(0, max_index)
-slope, intercept = np.polyfit(np.ravel(strain[linear_range]), np.ravel(stress[linear_range]), deg=1)
+slope, intercept = np.polyfit(strain[linear_range], stress[linear_range], deg=1)
 youngs_modulus = slope
 plt.plot(strain[linear_range], intercept + slope * strain[linear_range], color='red',label='linear regression' )
 plt.legend()
